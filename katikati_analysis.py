a404b686c8722cb71e372aa875213e736fc42e86
Alexander Simpson <as2388@cam.ac.uk>
2020-02-17 15:40:53 +0000

    Print the number of times each text was sent in the duration of the project

--- katikati_analysis.py
+++ katikati_analysis.py
@@ -1,10 +1,7 @@
 import argparse
 import json
 
-from core_data_modules.cleaners import PhoneCleaner, Codes
 from core_data_modules.logging import Logger
-from core_data_modules.traced_data.io import TracedDataJsonIO
-from dateutil.parser import isoparse
 from id_infrastructure.firestore_uuid_table import FirestoreUuidTable
 from rapid_pro_tools.rapid_pro_client import RapidProClient
 from storage.google_cloud import google_cloud_utils
@@ -26,7 +23,7 @@ if __name__ == "__main__":
 
     parser.add_argument("messages_json_input_path", metavar="messages-json-input-path",
                         help="Path to a JSONL file to read the TracedData of the messages data from")
-    
+
     args = parser.parse_args()
     user = args.user
     google_cloud_credentials_file_path = args.google_cloud_credentials_file_path
@@ -72,75 +69,15 @@ if __name__ == "__main__":
         raw_sent_messages.extend([msg for msg in project_messages
                                   if msg.direction == "out" and msg.urn.startswith("tel:")])
     log.info(f"{len(raw_sent_messages)} of the downloaded messages were outbound")
-    
+
     sent_message_counts = dict()  # of message text -> number of times that message was sent during the project
     for msg in raw_sent_messages:
         if msg.text not in sent_message_counts:
             sent_message_counts[msg.text] = 0
         sent_message_counts[msg.text] += 1
     log.info(f"{len(sent_message_counts)} unique texts were sent out during this project")
+    log.info("Number of times each messages was sent, message text:")
+    for text, count in sent_message_counts.items():
+        log.info(f"    {count:>4}, {text}")
 
-    # Create a list of de-identified outbound message objects containing the information we care about
-    phone_numbers = {PhoneCleaner.normalise_phone(msg.urn.split(":")[1]) for msg in raw_sent_messages}
-    phone_to_uuid_lut = phone_number_uuid_table.data_to_uuid_batch(phone_numbers)
-    sent_messages = []
-    for msg in raw_sent_messages:
-        if msg.sent_on is None:
-            log.warning(f"Found a message with a sent_on date of None, ignoring. Text was: {msg.text}")
-            continue
 
-        sent_messages.append({
-            "uid": phone_to_uuid_lut[PhoneCleaner.normalise_phone(msg.urn.split(":")[1])],
-            "text": msg.text,
-            "sent_on": msg.sent_on.isoformat(),
-            "direction": "out"
-        })
-
-    # Load the labelled project messages data
-    log.info(f"Loading messages from {messages_json_input_path}...")
-    with open(messages_json_input_path) as f:
-        traced_messages = TracedDataJsonIO.import_jsonl_to_traced_data_iterable(f)
-    log.info(f"Loaded {len(traced_messages)} labelled message objects")
-
-    received_messages = []
-    for msg in traced_messages:
-        if msg["consent_withdrawn"] == Codes.TRUE:
-            continue
-        
-        for coding_plan in PipelineConfiguration.RQA_CODING_PLANS:
-            labels = []
-            for cc in coding_plan.coding_configurations:
-                codes = [cc.code_scheme.get_code_with_code_id(label["CodeID"]) for label in msg[cc.coded_field]]
-                if len(codes) > 1 or codes[0].control_code != Codes.TRUE_MISSING:
-                    received_messages.append({
-                        "uid": msg["uid"],
-                        "text": msg[coding_plan.raw_field],
-                        "sent_on": msg["sent_on"],
-                        "direction": "in",
-                        "labels": msg[cc.coded_field]
-                    })
-
-    # Join the in- and out-bound messages
-    messages_by_individual = dict()  # of uuid -> message object
-    for msg in sent_messages + received_messages:
-        uid = msg["uid"]
-        if uid not in messages_by_individual:
-            messages_by_individual[uid] = []
-        messages_by_individual[uid].append(msg)
-
-    # for msg in sent_messages:
-    #     uid = msg["uid"]
-    #     if uid not in messages_by_individual:
-    #         messages_by_individual[uid] = []
-    #     messages_by_individual[uid].append(("out", msg))
-
-    # Within each group, sort the messages in the order that they were sent
-    for messages in messages_by_individual.values():
-        messages.sort(key=lambda msg: isoparse(msg["sent_on"]))
-
-    # for ind in messages_by_individual.values():
-    #     for msg in ind:
-    #         print(msg)
-
-    with open("test.json", "w") as f:
-        json.dump(messages_by_individual, f)
